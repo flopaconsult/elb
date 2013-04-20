72acdc1431135737a7889fe92911c4afd56293e0
Florin Popa <flopaconsult@gmail.com>
2013-04-20 23:59:07 +0300

    changes from florin-wrk on Sat Apr 20 23:59:07 EEST 2013

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@ default["elb"]["elb_version_name"]="ElasticLoadBalancing-1.0.17.0"
 default["elb"]["aws_elb_home"]="/opt/elb/#{default["elb"]["elb_version_name"]}"
 default["elb"]["scripts_home"]="/opt/elb"
 default["region"]="us-east-1"
-default["ec2"]["instance_id]="a"
+default["ec2"]["instance_id"]="a"
 
 
